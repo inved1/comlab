5ddba1e30792942141c167a1411b88b419b57f6e
dan10180 <dan10180@esri.com>
2020-01-10 09:47:56 +0100

    commit

---
+++ comlab/Source/comlab/comlabGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "comlabGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMLAB_API AcomlabGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
